c718330058583d8645482869b07524ea5ba87d13
G16 <stken79s@gmail.com>
2024-01-07 17:02:17 +0800

    Ken 1/7 1702

---
+++ eclipse_workplace/BigProject/src/main/java/com/web/store/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.web.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+	
+	@GetMapping("/Member")
+	public String member(Model model) {
+		return "Member/Member";
+	}
+
+	@GetMapping("/Love")
+	public String love(Model model) {
+		return "Member/Love";
+	}
+	
+	@GetMapping("/Activity_list")
+	public String activity_list(Model model) {
+		return "Member/Activity_list";
+	}
+}
