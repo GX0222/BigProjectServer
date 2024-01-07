862a4fa508ad75292394c5241f7a94d6928e525b
G16 <stken79s@gmail.com>
2024-01-07 19:57:04 +0800

    Ken 1/7 1957

--- eclipse_workplace/BigProject/src/main/java/com/web/store/controller/HomeController.java
+++ eclipse_workplace/BigProject/src/main/java/com/web/store/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.web.store.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
--- eclipse_workplace/BigProject/src/main/java/com/web/store/controller/MemberController.java
+++ eclipse_workplace/BigProject/src/main/java/com/web/store/controller/MemberController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
-	
+
 	@GetMapping("/Member")
 	public String member(Model model) {
 		return "Member/Member";
@@ -16,7 +16,7 @@ public class MemberController {
 	public String love(Model model) {
 		return "Member/Love";
 	}
-	
+
 	@GetMapping("/Activity_list")
 	public String activity_list(Model model) {
 		return "Member/Activity_list";
