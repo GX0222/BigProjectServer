558790ba73a86851081520d17b1278c5372ab391
ARrrr <arieljbb45@gmail.com>
2024-01-08 15:05:09 +0800

    ariel

--- _00_Data/event/db.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$servername = "192.168.12.123";
-$username = "root";
-$password = "root";
-$dbname = "shen"; 
-$port = "3306";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
-?>
