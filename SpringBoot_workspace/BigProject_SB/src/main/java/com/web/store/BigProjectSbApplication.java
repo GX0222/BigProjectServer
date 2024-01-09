a94e08f6bc7b681677b767504aaa739fb9b6f37c
G16 <stken79s@gmail.com>
2024-01-10 02:14:22 +0800

    1/10

---
+++ SpringBoot_workspace/BigProject_SB/src/main/java/com/web/store/BigProjectSbApplication.java
@@ -0,0 +1,13 @@
+package com.web.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigProjectSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigProjectSbApplication.class, args);
+	}
+
+}
