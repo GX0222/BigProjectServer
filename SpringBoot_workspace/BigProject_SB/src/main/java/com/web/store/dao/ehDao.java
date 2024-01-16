55d0040207169d3339671eee35a2ec60412437af
yourName1~ <richard.chyan@gmail.com>
2024-01-16 11:13:59 +0800

    EhDao change

--- SpringBoot_workspace/BigProject_SB/src/main/java/com/web/store/dao/ehDao.java
+++ SpringBoot_workspace/BigProject_SB/src/main/java/com/web/store/dao/ehDao.java
@@ -3,11 +3,12 @@ package com.web.store.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.web.store.model.ehBean;
+import com.web.store.model.EhBean;
 
-public interface ehDao extends JpaRepository<ehBean, Integer> {
-	List<ehBean> findAll();
-	
-	List<ehBean> findAllByClassId(Integer classId);
+public interface EhDao extends JpaRepository<EhBean, Integer> {
+	@Query("SELECT m FROM EhBean m WHERE m.event_id = :eventId")
+	public List<EhBean> findByEvent_id(@Param("eventId")Integer id);
 }
