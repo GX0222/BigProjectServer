f504e76a4e14bf41834e2364daeca110bf91da13
YEN <XXX@gmail.com>
2024-01-23 10:14:14 +0800

    YEN 01/23 10:14

---
+++ sql/members.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 
+-- 伺服器版本： 8.0.17
+-- PHP 版本： 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `bigproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
+  `account` varchar(255) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `login_delay_time` int(11) DEFAULT NULL,
+  `login_state` int(11) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 傾印資料表的資料 `members`
+--
+
+INSERT INTO `members` (`id`, `account`, `birthday`, `level`, `login_delay_time`, `login_state`, `mail`, `password`, `phone`, `username`) VALUES
+(1, 'Yen555', NULL, NULL, NULL, NULL, 'roccc555@gmail.com', '$2a$10$g.VdDalrmzrIuRINkr5L/uR4LiJpIHMwKZV7GwDyMsFdFnF2jU1Iq', '0912-345678', '張大帥');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
