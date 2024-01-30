4f466bf21d3422c7f5f664260249c79ced503f5e
GX <stken79s@gmail.com>
2024-01-30 16:31:50 +0800

    K Nb CLASS

---
+++ SpringBoot_workspace/BigProject_SB/src/main/resources/static/Tools/WebSocket.js
@@ -0,0 +1,28 @@
+
+// 創建 StompJs 客戶端並設置參數
+const client = new StompJs.Client({
+    brokerURL: 'ws://localhost:80/WebSocket',
+    debug: function (str) {
+        console.log(str);
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+});
+
+client.onConnect = function (frame) {
+    console.log('WebSocket connected!');
+    client.subscribe('/WSMessage/Mes', function (message) {
+        console.log('Received message: ' + message.body);
+    });
+};
+
+// 定義 onStompError 函數，用於處理 Stomp 錯誤
+client.onStompError = function (frame) {
+    console.log('Broker reported error: ' + frame.headers['message']);
+    console.log('Additional details: ' + frame.body);
+};
+
+// 啟動客戶端
+client.activate();
+
